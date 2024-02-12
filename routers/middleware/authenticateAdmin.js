85a41ff1146d34f1be98eaae21efb3ddb5a3c9cf
TMan253 <TMan253@yahoo.com>
2024-02-12 16:45:09 -0600

    Connected user login logic to DB

--- routers/middleware/authenticateAdmin.js
+++ routers/middleware/authenticateAdmin.js
@@ -1,41 +1,39 @@
 const jwt = require("jsonwebtoken");
+const User = require("../../db/models/user");
 
 // Export a middleware function that takes the conventional arguments.
 module.exports = (req, res, next) => {
-    // Extract the token from the Authorization request header.
-    const token = req.headers.authorization;
-    // Fetch the secret used to sign the JWT.
-    const secret = process.env.JWT_SECRET || "juicy";
+  // Extract the token from the Authorization request header.
+  const token = req.headers.authorization;
+  // Fetch the secret used to sign the JWT.
+  const secret = process.env.JWT_SECRET || "juicy";
+  // Fetch the admin key for admin verification.
+  const key = process.env.ADMIN_KEY || "21";
 
-    // Check whether a JWT is provided in the request header.
-    if(token) {
-        // Verify the JWT.
-        jwt.verify(token, secret, (err, decodedToken) => {
-            if (err || !decodedToken) {
-                // If no JWT present, return 401.
-                res.status(401).json({ message: "Not verified" });
-            } else {
-                // Token validated, so find the username in the database.
-                const user = {
-                    username: "testUser",
-                    password: "juicyboo",
-                    adminKey: 21
-                }
-                // Extract admin key from user object if it exists.
-                const adminKey = user?.adminKey?.toString() ?? "";
-                // Check whether admin key matches .env file config.
-                if (adminKey == process.env.ADMIN_KEY) {
-                    // Admin key matches - allow access.
-                    next();
-                } else {
-                    // Admin key does not match.
-                    res.status(401).json({ message: "Must be an administrator" });
-                }
-            }
-        });
-    } else {
-        // No JWT present.
-        res.status(401).json({ message: "No JWT" });
-    }
+  // Check whether a JWT is provided in the request header.
+  if (token) {
+    // Verify the JWT.
+    jwt.verify(token, secret, async (err, decodedToken) => {
+      if (err || !decodedToken) {
+        // If no JWT present, return 401.
+        res.status(401).json({ message: "Not verified" });
+      } else {
+        // Token validated, so find the username in the database.
+        const user = await User.findByUsername(decodedToken.username);
+        // Extract admin key from user object if it exists.
+        const adminKey = user?.adminKey?.toString() ?? "";
+        // Check whether admin key matches .env file config.
+        if (adminKey == process.env.ADMIN_KEY) {
+          // Admin key matches - allow access.
+          next();
+        } else {
+          // Admin key does not match.
+          res.status(401).json({ message: "Must be an administrator" });
+        }
+      }
+    });
+  } else {
+    // No JWT present.
+    res.status(401).json({ message: "No JWT" });
+  }
 };
-
